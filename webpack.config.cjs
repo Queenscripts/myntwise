f59e29ca0b554d1fa7e0facbc51b5d9ba5164f63
Queen <queennefertitishabazz@gmail.com>
2022-02-16 23:35:55 -0800

    2/16| webpack config debug for Heroku

---
+++ webpack.config.cjs
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports={
+    mode: 'production',
+    entry:'./src/index.js',
+    output:{
+        filename: 'index.js',
+        path: path.resolve(__dirname, 'static')
+    }, 
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$|jsx/,
+                include: [
+                    path.resolve(__dirname, 'src')
+                  ],
+                use: ['babel-loader'],
+            },
+            
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            hash: true,
+            filename: './templates/index.html' //relative to root of the application
+        })
+   ]
+}
