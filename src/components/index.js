ab61b35a1f7b92acdc1e4dc2424a86ddb52fb9db
Queen <queennefertitishabazz@gmail.com>
2022-02-17 19:18:19 -0800

    2/17| Force add index.js file

---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+export {default as About} from './About.jsx';
+export {default as Advice} from './Advice.jsx';
+export {default as Budget} from './Budget.jsx';
+export {default as Menu} from './Dashboard/Menu.jsx'
+export {default as Footer} from './Footer.jsx';
+export {default as Login} from './Login.jsx';
+
+export {default as Home} from './Home.jsx';
+export {default as Nav} from './Nav.jsx'
