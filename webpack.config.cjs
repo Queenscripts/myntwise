8fcda00d6c71c24ce064cbc482bfc6a243f9ddce
Queen <queennefertitishabazz@gmail.com>
2022-02-17 19:05:50 -0800

    2/17| webpack config debug for index.js

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,32 +1,29 @@
-import path from 'path';
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+import { resolve as _resolve } from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
-module.exports={
-    mode: 'production',
-    entry: path.resolve(__dirname, 'src/index.js'),
-    output:{
-        filename: 'index.js',
-        path: path.resolve(__dirname, 'static')
-    }, 
-    resolve: {
-        extensions: ['', '.js', '.jsx']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$|jsx/,
-                include: [
-                    path.resolve(__dirname, 'src')
-                  ],
-                use: ['babel-loader'],
-            },
-            
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            hash: true,
-            filename: './templates/index.html' //relative to root of the application
-        })
-   ]
-}
+export const mode = 'production';
+export const entry = _resolve(__dirname, 'src/index.js');
+export const output = {
+    filename: 'index.js',
+    path: _resolve(__dirname, 'static')
+};
+export const resolve = {
+    extensions: ['', '.js', '.jsx']
+};
+export const module = {
+    rules: [
+        {
+            test: /\.js$|jsx/,
+            include: [
+                _resolve(__dirname, 'src')
+            ],
+            use: ['babel-loader'],
+        },
+    ]
+};
+export const plugins = [
+    new HtmlWebpackPlugin({
+        hash: true,
+        filename: './templates/index.html' //relative to root of the application
+    })
+];
