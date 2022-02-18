18dc5b6ac2b21da58bfef32df95e25ea65f6d903
Queen <queennefertitishabazz@gmail.com>
2022-02-17 19:09:45 -0800

    2/17| Force add index.js file

---
+++ src/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Routes, Route, IndexRoute  } from 'react-router'
+
+import {
+    About, 
+    Advice, 
+    Budget,
+    Footer, 
+    Menu,
+    Login,
+    Home, 
+    Nav
+} from './components/index.js'; 
+import {BrowserRouter} from 'react-router-dom';
+import SignUp from './components/SignUp.jsx';
+
+function App(){
+    return(
+        <>
+        <BrowserRouter>
+            <Nav
+                user={user}
+            />
+            <Routes>
+                {/* <IndexRoute component = {Home} /> */}
+                <Route path="/" element={<Home/>} title={"Home"}></Route>
+                <Route exact path="/about" element={<About/>}></Route>
+                <Route exact path="/budgets" element={<Budget/>}></Route>
+                <Route path="/dashboard"  element={<Menu/>}></Route>
+                <Route exact path="/signup"  element={<SignUp/>} title="Signup"></Route>
+                <Route exact path="/login"  element={<Login/>} title="Login"></Route>
+            </Routes>
+            <Footer/>
+        </BrowserRouter>
+        </>
+
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
