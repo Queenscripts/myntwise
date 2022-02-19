1662e1842ba701b3770ef1b507bf4caf4a40e72e
Queen <queennefertitishabazz@gmail.com>
2022-02-19 14:30:55 -0800

    2/19| server test file refactor config migrations + manage

--- server.py
+++ server.py
@@ -485,7 +485,7 @@ if __name__ == "__main__":
     app.DEBUG_TB_INTERCEPT_REDIRECTS = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     app.config.from_object(Config)
-
+    db(app)
     connect_to_db(app)
     DebugToolbarExtension(app)
 
