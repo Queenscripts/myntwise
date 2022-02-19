e8b6932007afa078fe9b0f777862f9294f6ca958
Queen <queennefertitishabazz@gmail.com>
2022-02-19 15:51:15 -0800

    2/19| server test file refactor config migrations + manage-- track modifications-- .envrc

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ import os
 class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.urandom(24)
-    SQLALCHEMY_DATABASE_URI = os.environ['POSTGRES_URI']
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
