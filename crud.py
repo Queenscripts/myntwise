6939cf8e35c77800166b06f333245bf6c86ff624
Queen <queennefertitishabazz@gmail.com>
2022-02-19 16:45:46 -0800

    2/19| crud advice- paginate

--- crud.py
+++ crud.py
@@ -147,7 +147,7 @@ def filter_by_date(user_id, start_date, end_date):
 # CRUD FOR ADVICE
 def get_advice():
     """Get all advice"""
-    return model.Advice.query
+    return Advice.query
 
 def get_advice_by_name(advice_name):
     """Get all advice"""
