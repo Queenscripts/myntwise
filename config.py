115ead72d6dbbc4b423dbbface12d235dc327f10
Queen <queennefertitishabazz@gmail.com>
2022-02-26 14:11:56 -0800

    2/26| Pagination & CSS refactor on Menu Component

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ import os
 class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.urandom(24)
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL?sslmode=require').replace('postgres://', 'postgresql://')
