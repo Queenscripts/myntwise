b7be7728d063018c2545404ffdc4ea9d6c930072
Queen <queennefertitishabazz@gmail.com>
2022-02-26 16:10:07 -0800

    2/26| CSS refactor

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function SignUp(){
     return(
         <>
-        <div class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+        <div class="flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
     <div class="max-w-md w-full space-y-8">
       <div>
         <img class="mx-auto h-12 w-auto" src="static/favicon_io/favicon-32x32.png" alt="Workflow"/>
